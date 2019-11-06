52c7d90a10cba63b2b39b30fd2e07f96105bfcf7
Ross Brown <RBrown@spartaglobal.com>
2019-11-06 09:35:49 +0000

    I reuploaded my homework with a few changes

--- Week3Day2Homework/Week3Day2Homework/Program.cs
+++ Week3Day2Homework/Week3Day2Homework/Program.cs
@@ -24,16 +24,82 @@ namespace Week3Day2Homework
             var d = new Comparison();
             c.name = "Bob";
             d.name = "Carly";
-            Console.WriteLine("{0} compared to {1} is {2}", c.name, d.name, c.CompareByName(d));
-            Console.WriteLine("{0} compared to {1} is {2}", c.name, d.name, c.CompareByLength(d));
+            if (c.CompareByName(d) == -1)
+            {
+                Console.WriteLine("Alphabetically, {0} comes before {1}", c.name, d.name);
+            }
+            else if (c.CompareByName(d) == 0)
+            {
+                Console.WriteLine("Alphabetically, {0} is the same as {1}", c.name, d.name);
+            }
+            else
+            {
+                Console.WriteLine("Alphabetically, {0} comes after {1}", c.name, d.name);
+            }
+            if (c.CompareByLength(d) == -1)
+            {
+                Console.WriteLine(" {0} is shorter than {1}", c.name, d.name);
+            }
+            else if (c.CompareByLength(d) == 0)
+            {
+                Console.WriteLine("{0} is the same length as {1}", c.name, d.name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is longer than {1}", c.name, d.name);
+            }
             c.name = "Carly";
             d.name = "Carly";
-            Console.WriteLine("{0} compared to {1} is {2}", c.name, d.name, c.CompareByName(d));
-            Console.WriteLine("{0} compared to {1} is {2}", c.name, d.name, c.CompareByLength(d));
+            if (c.CompareByName(d) == -1)
+            {
+                Console.WriteLine("Alphabetically, {0} comes before {1}", c.name, d.name);
+            }
+            else if (c.CompareByName(d) == 0)
+            {
+                Console.WriteLine("Alphabetically, {0} is the same as {1}", c.name, d.name);
+            }
+            else
+            {
+                Console.WriteLine("Alphabetically, {0} comes after {1}", c.name, d.name);
+            }
+            if (c.CompareByLength(d) == -1)
+            {
+                Console.WriteLine(" {0} is shorter than {1}", c.name, d.name);
+            }
+            else if (c.CompareByLength(d) == 0)
+            {
+                Console.WriteLine("{0} is the same length as {1}", c.name, d.name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is longer than {1}", c.name, d.name);
+            }
             c.name = "Edward";
             d.name = "Carly";
-            Console.WriteLine("{0} compared to {1} is {2}", c.name, d.name, c.CompareByName(d));
-            Console.WriteLine("{0} compared to {1} is {2}", c.name, d.name, c.CompareByLength(d));
+            if (c.CompareByName(d) == -1)
+            {
+                Console.WriteLine("Alphabetically, {0} comes before {1}", c.name, d.name);
+            }
+            else if (c.CompareByName(d) == 0)
+            {
+                Console.WriteLine("Alphabetically, {0} is the same as {1}", c.name, d.name);
+            }
+            else
+            {
+                Console.WriteLine("Alphabetically, {0} comes after {1}", c.name, d.name);
+            }
+            if (c.CompareByLength(d) == -1)
+            {
+                Console.WriteLine(" {0} is shorter than {1}", c.name, d.name);
+            }
+            else if (c.CompareByLength(d) == 0)
+            {
+                Console.WriteLine("{0} is the same length as {1}", c.name, d.name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is longer than {1}", c.name, d.name);
+            }
         }
     }
     public class Item : IComparable
